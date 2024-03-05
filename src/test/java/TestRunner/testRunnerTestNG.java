beb8ceff933c803f3c48caae507de899c5fbf818
SHIVANSH KUMAR  YADAV <74641316+YadavShivansh@users.noreply.github.com>
2024-03-06 00:50:01 +0530

    Update testRunnerTestNG.java

--- src/test/java/TestRunner/testRunnerTestNG.java
+++ src/test/java/TestRunner/testRunnerTestNG.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 //@RunWith(Cucumber.class)
-@CucumberOptions(features = { "src//test//resources//Features//EndToEnd.feature" },
+@CucumberOptions(features = { "src//test//resources//Features" },
 
 		glue = "StepDefination",
 
